e8648f56b325ec06eeb02faf59978929982a4415
Niyati <niyatipatil828@gmail.com>
2026-07-04 16:09:06 +0530

    new commit

--- MyPortfolio/src/App.jsx
+++ MyPortfolio/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-import {Routes, Route } from 'react-router-dom';
+
 import {Navbar} from'./components/Navbar';
 import {Home}from'./components/Home';
 import About from'./components/About';
